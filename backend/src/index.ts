cf48ace50163923b4f91eb90a0fe7268874785f9
JavierMC90 <mtzcornejo66@gmail.com>
2021-04-13 17:31:56 -0500

    Init project of backend

---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+const app = express()
+const PORT = 3000
+app.get('/', (req, res) => res.send('Express + TypeScript'))
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+})
